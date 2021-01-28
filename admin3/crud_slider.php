3f1de03da02f556c9a521283557e2f5a7c1a5eef
Dz <josephgzum@gmail.com>
2021-01-28 13:42:19 -0500

    Sliders Insert update v 2.02

---
+++ admin3/crud_slider.php
@@ -0,0 +1,17 @@
+<?php
+include "contolador/conexion.php";
+$descripcion=$_REQUEST['descripcion'];
+$nombreimg=$_FILES['imagen']['name'];
+$archivo=$_FILES['imagen']['tmp_name'];
+$ruta="fotos";
+$ruta=$ruta."/".$nombreimg;
+
+move_uploaded_file($archivo,$ruta);
+$consulta="insert into sliders(descripcion,ruta_imagen,estado) values('".$descripcion."','".$ruta."',1)";
+$query=mysqli_query($conexion,$consulta);
+if($query){
+    header('location:../admin3/Slider.php');
+}else{
+    echo"Error";
+}
+?>
